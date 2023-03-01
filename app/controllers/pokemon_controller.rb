730baeab3002d108bf8c6599aadea9e9a88b1e40
Noa Kim <51217131+noanonoa@users.noreply.github.com>
2023-03-01 15:43:07 -0600

    3rd iteration (#3)
    * add httparty and json gems
    
    * add pokemon inflection
    
    * rails generate controller pokemon index
    
    * rails generate model pokemon name front_sprite
    
    * rails db:migrate
    
    * mock a pokemon
    
    * rails generate controller pokedex index

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,4 +1,7 @@
 class PokemonController < ApplicationController
   def index
+    # @pokemon = Pokemon.all
+    @pokemon = [{ :name => "bulbasaur",
+                  :front_sprite => "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" }]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'pokemon' => 'pokemon#index'
+  root 'pokedex#index'
+  get 'pokemon/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
