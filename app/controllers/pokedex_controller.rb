9bfdb45edc12a932b7fb1c36379ba6c205066365
Noa Kim <51217131+noanonoa@users.noreply.github.com>
2023-03-09 15:50:57 -0600

    search for pokemon and show results

--- app/controllers/pokedex_controller.rb
+++ app/controllers/pokedex_controller.rb
@@ -1,6 +1,5 @@
 class PokedexController < ApplicationController
-<<<<<<< HEAD
-  POKE_API_URL="https://pokeapi.co/api/v2/pokemon/"
+  POKE_API_URL = "https://pokeapi.co/api/v2/pokemon/"
 
   def index
   end
@@ -9,8 +8,4 @@ class PokedexController < ApplicationController
     response = HTTParty.get(POKE_API_URL + params[:pokemon])
     @pokemon = JSON(response.body)
   end
-=======
-  def index
-  end
->>>>>>> d87266d (generate pokemon controller)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 <<<<<<< HEAD
+<<<<<<< HEAD
   root "pokedex#index"
   get 'pokedex/index'
   get 'pokemon/index'
@@ -12,4 +13,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "pokedex#index"
 >>>>>>> d87266d (generate pokemon controller)
+=======
+  root "pokedex#index"
+  get 'pokedex/index'
+  get 'pokemon/index'
+  post '/search' => 'pokedex#search'
+>>>>>>> ca6c3bc (search for pokemon and show results)
 end
