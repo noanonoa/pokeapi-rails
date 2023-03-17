2076688ad82b0867eee4135cc1ca06e2007227af
Noa Kim <51217131+noanonoa@users.noreply.github.com>
2023-03-17 09:26:51 -0500

    adds pokemon search capability

--- app/controllers/pokedex_controller.rb
+++ app/controllers/pokedex_controller.rb
@@ -1,4 +1,8 @@
 class PokedexController < ApplicationController
+  POKE_API_URL = 'https://pokeapi.co/api/v2/pokemon/'
+
   def index
+    response = HTTParty.get(POKE_API_URL + params[:poke_name])
+    @pokemon = JSON.parse(response.body)
   end
 end
