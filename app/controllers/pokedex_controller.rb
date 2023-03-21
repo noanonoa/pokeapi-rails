319781fe1e9162c5ce74a9234f842bc05e869694
Noa Kim <51217131+noanonoa@users.noreply.github.com>
2023-03-21 15:20:22 -0500

    rails g controller pokedex index

--- app/controllers/pokedex_controller.rb
+++ app/controllers/pokedex_controller.rb
@@ -1,8 +1,4 @@
 class PokedexController < ApplicationController
-  POKE_API_URL = 'https://pokeapi.co/api/v2/pokemon/'
-
   def index
-    response = HTTParty.get(POKE_API_URL + params[:poke_name])
-    @pokemon = JSON.parse(response.body)
   end
 end
