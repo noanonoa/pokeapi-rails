592662bc688c90da1a7fcc13521411d4af959c0d
Noa Kim <51217131+noanonoa@users.noreply.github.com>
2023-03-21 16:35:27 -0500

    7th iteration (#7)

--- app/controllers/pokedex_controller.rb
+++ app/controllers/pokedex_controller.rb
@@ -1,7 +1,10 @@
 class PokedexController < ApplicationController
-  POKE_API_URL = 'https://pokeapi.co/api/v2/pokemon/'
+  POKE_API_URL = "https://pokeapi.co/api/v2/pokemon/"
 
   def index
+  end
+
+  def search
     response = HTTParty.get(POKE_API_URL + params[:poke_name])
     @pokemon = JSON.parse(response.body)
   end
