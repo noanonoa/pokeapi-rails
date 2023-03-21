9a67369d2a723ece47903cafef00dccbd8be74f7
Noa Kim <51217131+noanonoa@users.noreply.github.com>
2023-03-21 15:53:22 -0500

    add pokemon search functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'pokedex/index'
   get 'pokemon/index'
+  get '/search' => 'pokedex#search'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pokedex#index"
 end
