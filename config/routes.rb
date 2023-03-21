d0bf8a319495209a605195cee74a2d8d4f0cec2d
Noa Kim <51217131+noanonoa@users.noreply.github.com>
2023-03-21 15:19:31 -0500

    rails g controller pokemon index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  root 'pokedex#index'
-  get 'pokedex/index'
   get 'pokemon/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
