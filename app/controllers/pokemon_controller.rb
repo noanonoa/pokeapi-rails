338c0c13dc5d1e6388c56402a4f86c4936d4c631
Noa Kim <51217131+noanonoa@users.noreply.github.com>
2023-03-23 15:43:24 -0500

    8th iteration (#8)

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,4 +1,13 @@
 class PokemonController < ApplicationController
   def index
+    @pokemon = Pokemon.all
+  end
+
+  def catch
+    Pokemon.find_or_create_by( external_id: params[:external_id],
+                               name: params[:poke_name],
+                               sprite_url: params[:sprite_url] )
+
+    redirect_to pokemon_path
   end
 end
