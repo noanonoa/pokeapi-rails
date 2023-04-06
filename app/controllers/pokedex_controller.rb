4702458dc1a04e331e84049a06b3cd59a4dcc1d8
Noa Kim <51217131+noanonoa@users.noreply.github.com>
2023-04-06 10:39:31 -0500

    shows pokemon search results

--- app/controllers/pokedex_controller.rb
+++ app/controllers/pokedex_controller.rb
@@ -1,8 +1,11 @@
 class PokedexController < ApplicationController
+  POKE_API_URL = "https://pokeapi.co/api/v2/pokemon/"
   def index
   end
 
   def search
-    fail
+    # params[:name]
+    response = HTTParty.get(POKE_API_URL + params[:name])
+    @pokemon = JSON.parse(response.body)
   end
 end
